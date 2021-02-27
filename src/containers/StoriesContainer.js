1795425f6c5c00f1c8f0e0dcc0e631a3ebbb908c
Wilson Luong <wilsonbluong@gmail.com>
2021-02-27 10:52:18 -0800

    initial commit

---
+++ src/containers/StoriesContainer.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { getStoryIds } from "../services/hnApi";
+import { Story } from "../components/Story";
+
+import {
+  GlobalStyle,
+  StoriesContainerWrapper,
+} from "../styles/StoriesContainerStyles";
+
+import { useInfiniteScroll } from "../hooks/useInfiniteScroll";
+
+export const StoriesContainer = () => {
+  // const { count } = useInfiniteScroll();
+  const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    getStoryIds().then((data) => setStoryIds(data));
+  }, []);
+
+  return (
+    <>
+      <GlobalStyle />
+      <StoriesContainerWrapper>
+        <h1>Hacker News Stories</h1>
+        {storyIds.map((storyId) => (
+          <Story key={storyId} storyId={storyId} />
+        ))}
+      </StoriesContainerWrapper>
+    </>
+  );
+};
